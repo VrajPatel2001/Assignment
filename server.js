b43ab9c872f628868efad201eea70290d7ebef5a
vnpatel17 <vrajnp2001@gmail.com>
2021-03-09 03:08:49 -0500

    signUp validation

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
-var exphbs  = require('express-handlebars');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+
 const app = express();
 
 
@@ -31,6 +34,54 @@ app.get("/movies",(req,res)=>
 app.get("/SignUp",(req,res)=>
 {
     res.render("signUp"); 
+})
+
+app.post("/SignUp",(req,res)=>
+{
+    const nameError = [];
+
+    const name = req.body.name;
+    if(name == "")
+    {
+        nameError.push("You must enter the name");
+    }
+    else if(name.length <2 || name.length >12)
+    {
+        nameError.push("Your name must be between 2 and 12 characters long");
+    }
+
+    const emailError = [];
+    const email = req.body.email;
+    if(email == "")
+    {
+        emailError.push("You must enter the name");
+    }
+
+
+    const passError = [];
+    const password = req.body.password;
+    const isNumber=0;
+    for(let i=0;i<password.length;i++)
+    {
+
+        if(password[i] == [0-9])
+        {
+            isNumber = 1;
+        } 
+    }
+    if(passError == "")
+    {
+        passError.push("You must enter the name");
+    }
+    else if(isNumber == 0)
+    {
+        passError.push("Your password should contain atleast one number");
+    }
+
+    if(nameError.length>0 || emailError.length>0 || passError.length>0)
+    {
+        res.render("signUp",{nameError,emailError,passError}); 
+    }
 })
 
 app.get("/Login",(req,res)=>
