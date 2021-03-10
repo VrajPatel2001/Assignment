587b970da42f88cd3b6b242d1438f6f7c46df162
vnpatel17 <vrajnp2001@gmail.com>
2021-03-10 00:05:03 -0500

    Website after complete validation with email service

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -21,19 +21,19 @@ console.log(featureTvShows[0].name);
 
 app.get("/",(req,res)=>
 {
-    res.render("index",{featureMovies,movies,tvShows,featureTvShows});
+    res.render("index",{title:"V-TV",featureMovies,movies,tvShows,featureTvShows});
 })
 
 app.get("/movies",(req,res)=>
 {
-    res.render("movie",{movies,tvShows});
+    res.render("movie",{title:"Movies And TV-shows",movies,tvShows});
 })
 
 
 
 app.get("/SignUp",(req,res)=>
 {
-    res.render("signUp"); 
+    res.render("signUp",{title:"SignUP"}); 
 })
 
 app.post("/SignUp",(req,res)=>
@@ -41,37 +41,39 @@ app.post("/SignUp",(req,res)=>
     const nameError = [];
 
     const name = req.body.name;
+    
     if(name == "")
     {
         nameError.push("You must enter the name");
     }
-    else if(name.length <2 || name.length >12)
+    else if(name.length <2 )
     {
-        nameError.push("Your name must be between 2 and 12 characters long");
+        nameError.push("Your name must be between 2 or more characters long");
     }
 
     const emailError = [];
-    const email = req.body.email;
+   const email = req.body.email;
     if(email == "")
     {
-        emailError.push("You must enter the name");
+        emailError.push("You must enter the email");
     }
 
 
     const passError = [];
     const password = req.body.password;
-    const isNumber=0;
+    let isNumber=0;
     for(let i=0;i<password.length;i++)
     {
-
-        if(password[i] == [0-9])
+        console.log(password[i]);
+        const patt = /[0-9]/g;
+        if(patt.test(password[i]))
         {
             isNumber = 1;
         } 
     }
-    if(passError == "")
+    if(password == "")
     {
-        passError.push("You must enter the name");
+        passError.push("You must enter the password");
     }
     else if(isNumber == 0)
     {
@@ -80,13 +82,73 @@ app.post("/SignUp",(req,res)=>
 
     if(nameError.length>0 || emailError.length>0 || passError.length>0)
     {
-        res.render("signUp",{nameError,emailError,passError}); 
+        
+        for(let i=0;i<passError.length;i++)
+        {
+            console.log(passError[i]);
+        }
+        
+        res.render("signUp",{title:"SignUP",nameError,emailError,passError}); 
+    }
+    else{
+
+
+        const sgMail = require('@sendgrid/mail')
+        //('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')
+ sgMail.setApiKey('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')
+const msg = {
+  to: email, // Change to your recipient
+  from: 'vrajnp2001@gmail.com', // Change to your verified sender
+  subject: 'V-TV',
+  text: 'Hi, Welcome to V-TV',
+  html: '<strong>Thanks for sign up on V-TV. <br>Enjoy Free movies only on V-TV. <br>  </strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+        res.render("dashboard",{title:"Dashboard",name:name});
     }
 })
 
 app.get("/Login",(req,res)=>
 {
-    res.render("login");
+    res.render("login",{title:"Login"});
+})
+
+app.post("/Login",(req,res)=>
+{
+    const nameError = [];
+
+    const name = req.body.username;
+    
+    if(name == "")
+    {
+        nameError.push("You must enter the name");
+    }
+   
+
+
+    const passError = [];
+    const password = req.body.password;
+    
+    if(password == "")
+    {
+        passError.push("You must enter the password");
+    }
+    
+
+    if(nameError.length>0 || passError.length>0)
+    {
+        res.render("login",{title:"Login",nameError,passError}); 
+    }
+    else{
+        res.render("index",{title:"V-TV"});
+    }
 })
 
 app.get("/description/:id",(req,res)=>
@@ -94,7 +156,7 @@ app.get("/description/:id",(req,res)=>
     
    let id = req.params.id;
    const movie = data.getAMovieOrTv(id);
-    res.render("description",{movie});
+    res.render("description",{title:"Description",movie});
 })
 
 
